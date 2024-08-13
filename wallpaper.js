6395b4ea8c02be1c3e7955a9b46d93b73d921733
ExtemporaneousnessOne <133890411+ExtemporaneousnessOne@users.noreply.github.com>
2024-08-13 19:53:38 -0400

    Update wallpaper.js

--- wallpaper.js
+++ wallpaper.js
@@ -1,12 +1,1 @@
-function updateDateTime() {
-	const now = new Date();
-	const hour = now.getHours().toString().padStart(2, '0');
-	const minute = now.getMinutes().toString().padStart(2, '0');
-	document.getElementById("hour").textContent = hour;
-	document.getElementById("minute").textContent = minute;
-	
-	document.getElementById("colon").textContent = ":";
-}
-	
-const myInterval = setInterval(updateDateTime, 1000);
 
